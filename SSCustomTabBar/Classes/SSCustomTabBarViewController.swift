d7f81791b038ba0c9f76b6c4d0c4c3a82419b157
vitor-moymer <vitor@moymer.com>
2019-11-06 17:50:52 -0300

    Update SSCustomTabBarViewController.swift

--- SSCustomTabBar/Classes/SSCustomTabBarViewController.swift
+++ SSCustomTabBar/Classes/SSCustomTabBarViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 /// Default index value for priviousSelectedIndex
 private let defaultIndexValue = -1
 
-open public class SSCustomTabBarViewController: UITabBarController {
+open class SSCustomTabBarViewController: UITabBarController {
     
     
     /// Tabbar height
